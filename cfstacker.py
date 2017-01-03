ba8e9dc158f76233952edf106fbc15c16ec069d2
gabriell nascimento <gabriellhrn@gmail.com>
2017-01-02 22:20:14 -0200

    add command line arguments to cfstacker.py

--- cfstacker.py
+++ cfstacker.py
@@ -10,6 +10,24 @@ def parse_args():
     parser = argparse.ArgumentParser(prog=PROG,
             description='Manages CloudFormation template files (YAML or JSON) stored at "templates/"')
 
+    parser.add_argument('-V', '--version', action='version', version='%s version %s' % (PROG, VERSION))
+
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument('-b', '--boilerplate', action='store_true',
+            help='generate boilerplate for TEMPLATE')
+    group.add_argument('-c', '--create', action='store_true',
+            help='create an ecs stack based on TEMPLATE')
+    group.add_argument('-d', '--delete', action='store_true',
+            help='delete TEMPLATE\'s stack')
+    group.add_argument('-u', '--update', action='store_true',
+            help='update TEMPLATE\'s stack')
+
+    parser.add_argument('template', metavar='TEMPLATE', type=str,
+            help='name of a CloudFormation file (YAML or JSON) to be managed')
+    parser.add_argument('parameters', metavar='PARAMETERS', type=str, nargs='?',
+            help='name of a json file containing parameters to be used in TEMPLATE (optional)')
+
     return parser.parse_args()
 
 # main
